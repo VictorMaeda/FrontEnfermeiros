f64bdedee9528129a82e20cfe448b18da127bf49
Victor-Maeda <victormaeda.javavespertino@gmail.com>
2023-09-21 16:57:30 -0400

    TesteCommit

---
+++ src/Teste.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Teste = () => {
+  return (
+    <div>Ola mundo</div>
+  )
+}
+
+export default Teste
--- src/services/api.js
+++ src/services/api.js
@@ -4,6 +4,5 @@ export const api = axios.create({
     baseURL: 'http://localhost:8080',
     headers: {
         "Content-Type": 'application/json',
-        
     }
 });
