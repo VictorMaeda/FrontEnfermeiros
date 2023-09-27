6d87a4fbe865c8e86c30787d48d904e531db1766
VictorMaeda <victor.chinen@eaportal.org>
2023-09-26 22:08:41 -0400

    dd

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
 import Plantoes from './Pages/Plantoes';
 import Login from './Pages/Login';
@@ -8,12 +8,16 @@ import Enfermeiros from './Pages/Enfermeiros';
 import DashBoard from './Pages/DashBoard';
 
 function App() {
+
+const [token, setToken] = useState(sessionStorage.getItem("token"))
+
+
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Login />} />
-          <Route path='/Plantoes' element={<Plantoes />} />
+          <Route path='/Plantoes' element={token ? <Plantoes /> : <Navigate to={'/'} />} />
           <Route path='/Enfermeiros' element={<Enfermeiros />} />
           <Route path='/DashBoard' element={<DashBoard />} />
         </Routes>
